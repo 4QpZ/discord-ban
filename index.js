3cbe6171d9883baf6f9c90f8f68a5c7121baf1d3
4QpZ <53709913+4QpZ@users.noreply.github.com>
2019-09-09 23:06:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content.startsWith('!ban')) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.ban({
+          reason: 'insert reason here',
+        }).then(() => {
+          message.reply(`Successfully banned ${user.tag}`);
+        }).catch(err => {
+          message.reply('I was unable to ban the user');
+          console.error(err);
+        });
+      } else {
+        message.reply('That user isn\'t in this guild!');
+      }
+    } else {
+      message.reply('You didn\'t mention the user to ban!');
+    }
+  }
+});
+
+client.login('your token here');
